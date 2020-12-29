2bae26223983963bfd0d57c76b519e6801327a45
Malachi <malachi.watkins3535@gmail.com>
2020-12-29 13:26:47 +0000

    WIP

--- main.py
+++ main.py
@@ -15,10 +15,21 @@ jewelsCollection = Itemdb["Jewels"]
 mapsCollection = Itemdb["Maps"]
 weaponsCollection = Itemdb["weapons"]
 armourCollection = Itemdb["armour"]
-flaskCollection = Itemdb["flasks"]
+
+# Finds all the buy orders that the user entered and puts them in the list
 while True:
-    post_list = []
-    # Finds all the buy orders that the user entered and puts them in the list
+    m = '0'
     for doc in buyorderCollection.find({'custom_id': custom_id}):
-        post_list.append(doc)
-# this needs to search for post that match the buy order if they do break and alert the user
+        item_collection = doc['item_type']
+        item_name = doc['item_name']
+        for matching_doc in currencyCollection.find({'typeLine': item_name}):
+
+            wisper = '@' + matching_doc['accountName'] + ' Hi, I would like to buy your ' + matching_doc['typeLine'] + \
+                ' listed in League (stash tab ' + matching_doc['stashname'] + \
+                '; position ' + 'x:' + str(matching_doc['x']) + \
+                ' ' + 'y:' + str(matching_doc['y']) + ')'
+            m = 'Found match!'
+            # replace this with GUI
+    if m == 'Found match!':
+        print(wisper)
+        pass
