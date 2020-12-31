dab60b76931b88d732f4ee2ba03a6c271839f477
Malachi <malachi.watkins3535@gmail.com>
2020-12-31 01:41:46 +0000

    WIP

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import pymongo
 from pymongo import MongoClient
-import pprint
 from ac import AC
 import time
 import json
-custom_id = 'malachi_custom-id'  # custom id
+
 cluster = MongoClient(AC)
 Itemdb = cluster["POE_DOCS"]
 currencyCollection = Itemdb["Currency"]
@@ -17,25 +16,28 @@ mapsCollection = Itemdb["Maps"]
 weaponsCollection = Itemdb["weapons"]
 armourCollection = Itemdb["armour"]
 
-
-# Finds all the buy orders that the user entered and puts them in the list
+stop_button = ''  # change this to when a stop button is pressed
+custom_id = 'malachi_custom-id'  # custom id
 button_pressed = 'PRESSED'  # change this to when a button is pressed
 if button_pressed == 'PRESSED':
     loop = True
-    cached_results = []
+    cached_results = []  # stores all the whispers to remove any dupes
     while loop == True:
-
+        # finds the buy order params using your custom id
         for doc in Itemdb["buyorder"].find({'custom_id': custom_id}):
-            # currencyCollection
             item_collection, item_name, item_price = doc['item_type'], doc['item_name'], doc['item_price']
-
+            # finds all the documents with matching params from the users buy order
             for matching_doc in Itemdb[item_collection].find({'typeLine': 'Blacksmith\'s Whetstone'}):
-                print('yeet')
+                # creates a whisper when it finds a mathching post
                 whisper = '@' + matching_doc['accountName'] + ' Hi, I would like to buy your ' + matching_doc['typeLine'] + \
                     ' listed in League (stash tab ' + matching_doc['stashname'] + \
                     '; position ' + 'x:' + str(matching_doc['x']) + \
                     ' ' + 'y:' + str(matching_doc['y']) + ')'
 
-                if whisper not in cached_results:
-                    print(whisper)
+                if whisper not in cached_results:  # checks if the post is a dupe if it is nothing happens if not appends it to a list
+                    # this is where GUI alet goes
+                    print('Found a match!')
                     cached_results.append(whisper)
+        # if the stop button is pressed stop the loop
+        if stop_button == 'PRESSED':
+            loop = False
